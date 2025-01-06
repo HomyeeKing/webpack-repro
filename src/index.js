96711c9882ca23acd367e5cc4f86c66e809fa850
HomyeeKing <HomyeeKing@gmail.com>
2025-01-06 16:27:50 +0800

    chore: save

--- src/index.js
+++ src/index.js
@@ -1,14 +1,2 @@
-import styles from './style.less';
-import * as lodash from 'lodash-es';
-import testJson from 'virtual:test';
-import { bar } from './another-module';
-import foo from './foo';
-console.log('foo', foo);
-
-console.log('testJson', testJson);
-console.log('bar', bar);
-console.log('lodash', lodash);
-const element = document.createElement('div');
-element.className = styles['skill-card-group-homepage']; // 使用 CSS Modules 中的类名
-element.textContent = 'Hello, Webpack with LESS and CSS Modules!';
-document.body.appendChild(element);
+import * as React from 'react';
+console.log('React', React.createElement);
--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ const autoprefixer = require('autoprefixer');
 const VirtualPlugin = require('./plugins/virtualPlugin');
 const VirtualPlugin1 = require('./plugins/vplugin1');
 // const VirtualPlugin = require('./unplugins/virtualPlugin');
+const esbuild = require('esbuild');
 
 const toPascalCase = (str) =>
   str
@@ -25,6 +26,28 @@ module.exports = {
   },
   module: {
     rules: [
+      {
+        //   // Match `.js`, `.jsx`, `.ts` or `.tsx` files
+        test: /\.m?[jt]sx?$/,
+        loader: 'esbuild-loader',
+        // available options: https://github.com/evanw/esbuild/blob/88821b7e7d46737f633120f91c65f662eace0bcf/lib/shared/types.ts#L158-L172
+        options: {
+          target: 'ESNext',
+          format: 'esm',
+          loader: 'js',
+          sourcemap: false,
+          tsconfigRaw: JSON.stringify({
+            compilerOptions: {
+              esModuleInterop: true,
+              isolatedModules: true,
+              skipLibCheck: true,
+            },
+          }),
+          implementation: esbuild,
+          banner:
+            "import { createRequire } from 'module';const require = createRequire(import.meta.url);",
+        },
+      },
       {
         test: /\.less$/, // 匹配所有 LESS 文件
         use: [
@@ -62,7 +85,7 @@ module.exports = {
   },
 
   plugins: [
-    new VirtualPlugin(),
+    // new VirtualPlugin(),
     // new VirtualPlugin1({
     //   'virutal:test': 'export const a = "default value";',
     // }),
